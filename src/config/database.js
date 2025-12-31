ae91f72137a559620685fcb6f8ed5af750eeafa0
Nikku308 <nikitashelke30@gmail.com>
2025-12-31 10:39:36 +0530

    -a

--- src/config/database.js
+++ src/config/database.js
@@ -1,20 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI is missing");
-    }
-
     console.log("Connecting to MongoDB...");
-
-    await mongoose.connect(process.env.MONGO_URI, {
-      dbName: "Cluster0", // optional, safe
-    });
-
-    console.log('✅ MongoDB Connected');
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB Connected");
   } catch (error) {
-    console.error('❌ MongoDB Error:', error.message);
+    console.error("❌ MongoDB Error:", error.message);
     process.exit(1);
   }
 };
